eab1d73ba4b1dee44d190efad9f1dc8c90e775b4
Microsoft <121215879+Azansongone@users.noreply.github.com>
2023-02-20 13:45:58 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,3 @@
 export LD_LIBRARY_PATH=${ROOT_DIR}/proprietary/llvm-arm-toolchain-ship/8.0/lib:$LD_LIBRARY_PATH
-export DTC_EXT=${ROOT_DIR}/prebuilts-master/misc/linux-x86/dtc/dtc
-export DTC_OVERLAY_TEST_EXT=${ROOT_DIR}/prebuilts-master/misc/linux-x86/libufdt/ufdt_apply_overlay
+export DTC_EXT=${ROOT_DIR}/prebuilts/misc/linux-x86/dtc/dtc
+export DTC_OVERLAY_TEST_EXT=${ROOT_DIR}/prebuilts/misc/linux-x86/libufdt/ufdt_apply_overlay
