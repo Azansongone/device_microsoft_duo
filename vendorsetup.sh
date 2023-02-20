c251e21f7d282c7874c542890cf7f1a89c687cfc
Microsoft <121215879+Azansongone@users.noreply.github.com>
2023-02-20 13:45:43 +0800

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,3 @@
-export BUILD_CONFIG=./kernel/msm-4.14/build.config.perf-config
+export LD_LIBRARY_PATH=${ROOT_DIR}/proprietary/llvm-arm-toolchain-ship/8.0/lib:$LD_LIBRARY_PATH
+export DTC_EXT=${ROOT_DIR}/prebuilts-master/misc/linux-x86/dtc/dtc
+export DTC_OVERLAY_TEST_EXT=${ROOT_DIR}/prebuilts-master/misc/linux-x86/libufdt/ufdt_apply_overlay
